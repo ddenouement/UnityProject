3ad2f7447045a49d275b7d9fdc7965eac9f76c52
Julia Aleksandrova <julialex38@gmail.com>
2018-05-20 12:13:59 +0300

    first try

---
+++ Assets/HeroFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+    public HeroRabbitGood rabbit;
+	// Use this for initialization
+	void Start () {
+			}
+	
+	// Update is called once per frame
+	void Update () {
+        Transform rabit_transform = rabbit.transform;
+        Transform camera_transform = this.transform;
+        Vector3 rab_pos = rabit_transform.position;
+        Vector3 cam_pos = camera_transform.position;
+        cam_pos.x = rab_pos.x;
+          cam_pos.y = rab_pos.y;
+          camera_transform.position = rabit_transform.position;
+	}
+}
